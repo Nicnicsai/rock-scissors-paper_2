91c38f2dafc546fc4858651e914e697ce8690b1e
Your Name <you@example.com>
2019-09-25 11:10:39 +0200

    adding scores

--- game_2_rock.js
+++ game_2_rock.js
@@ -6,6 +6,25 @@
         "assets/paper.png",
     ];
 
+    var youScore = 0;
+
+    function incrementCountYou(){
+        youScore++;
+    }
+
+    var computerScore = 0;
+
+    function incrementCountComputer(){
+        computerScore++;
+    }
+
+    function restart() {
+        document.getElementById("img-you").style.backgroundColor = "#75FAC7";
+        document.getElementById("img-computer").style.backgroundColor = "#75FAC7";
+        document.getElementById("GameBg").style.backgroundColor = "#75FAC7";
+        confetti.stop();
+    }
+
 
     var imgSourcePlayer = document.getElementById("img-you");
     var imgSourceComputer = document.getElementById("img-computer");
@@ -14,6 +33,7 @@
         button.addEventListener('click', function () {
 
             var chosenButton = button.getAttribute("id");
+            restart();
 
             if (chosenButton === "scissors-btn") {
                 imgSourcePlayer.setAttribute("src", gallery[0]);
@@ -29,6 +49,7 @@
 
     document.getElementById("play-btn").addEventListener("click", function () {
 
+        restart();
 
         imgSourceComputer.setAttribute("src", gallery[Math.floor(Math.random() * 3)]);
 
@@ -40,17 +61,30 @@
             || (player === "assets/paper.png" && computer === "assets/rock.png")) {
             document.getElementById("winner-result").innerHTML = "Winner: You!!!";
             document.getElementById("img-you").style.backgroundColor = "#33C191";
-            confetti.start();
+            incrementCountYou();
+            document.getElementById("youScore").innerHTML = "You: " + youScore;
+            if (youScore === 3) {
+                confetti.start();
+            }
+
+
 
         } else if ((player === "assets/scissors.png" && computer === "assets/rock.png")
             || (player === "assets/rock.png" && computer === "assets/paper.png")
             || (player === "assets/paper.png" && computer === "assets/scissors.png")) {
             document.getElementById("winner-result").innerHTML = "Winner: Evil Computer";
             document.getElementById("img-computer").style.backgroundColor = "#33C191";
+            incrementCountComputer();
+            document.getElementById("computerScores").innerHTML = "Computer: " + computerScore;
+
+            if (computerScore === 3) {
+                document.getElementById("GameBg").style.backgroundColor = "#c85733";
+            }
 
         } else {
             document.getElementById("winner-result").innerHTML = "Winner: Even!";
         }
+
     });
 
 
